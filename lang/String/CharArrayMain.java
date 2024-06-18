654db8905c4e00199e9ad085099d38508421474d
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-06-18 17:40:38 +0900

    Create CharArrayMain.java

---
+++ lang/String/CharArrayMain.java
@@ -0,0 +1,13 @@
+package lang.String;
+
+public class CharArrayMain {
+    // Char를 이용해서 hello 출력
+    // String를 이용해서 hello 출력
+    public static void main(String[] args) {
+        char[] carr = {'h','e','l','l','o'};
+        String str = "hello";
+
+        System.out.println(carr);
+        System.out.println(str);
+    }
+}
