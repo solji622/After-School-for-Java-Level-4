858c30b5fb2a3716efefb227376bf92fabb57da3
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-06-25 17:39:32 +0900

    Create Ex1_roomNum.java

---
+++ lang/wrapper/exam/Ex1_roomNum.java
@@ -0,0 +1,27 @@
+package lang.wrapper.exam;
+
+import java.util.Scanner;
+
+public class Ex1_roomNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] cnt = new int[10];
+
+        while (N > 0) {
+            cnt[N % 10]++;
+            N = N / 10;
+        }
+
+        int ans = (cnt[6] + cnt[9] + 1) / 2;
+        for (int i = 0; i < 9; i++) {
+            if (i != 6) {
+                ans = Math.max(ans, cnt[i]);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
