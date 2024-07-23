bc663d18a2884ca7bf67f4a2cccd6bde6c94a834
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-07-23 19:26:10 +0900

    Add files via upload

--- review/abs/detector/AdvancedFireDetector.java
+++ review/abs/detector/AdvancedFireDetector.java
@@ -3,6 +3,6 @@ package review.abs.detector;
 public class AdvancedFireDetector implements Detectable{
     @Override
     public void detect() {
-        System.out.println("일반 성능으로 화재를 감지합니다.");
+        System.out.println("향상된 성능으로 화재를 감지합니다.");
     }
 }
