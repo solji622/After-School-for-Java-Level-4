92b2647fe6b60195e7fcf48f2698d36f3a1d7ecf
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-07-23 19:26:52 +0900

    Add files via upload

--- review/abs/ReviewInterface.java
+++ review/abs/ReviewInterface.java
@@ -2,6 +2,7 @@ package review.abs;
 
 import review.abs.camera.FactoryCam;
 import review.abs.detector.AdvancedFireDetector;
+import review.abs.detector.FireDetector;
 import review.abs.reporter.NormalReporter;
 import review.abs.reporter.VideoReporter;
 
@@ -10,12 +11,21 @@ public class ReviewInterface {
         NormalReporter normalReporter = new NormalReporter();
         VideoReporter videoReporter = new VideoReporter();
 
+        normalReporter.report();
+        videoReporter.report();
+
+        FireDetector fireDetector = new FireDetector();
         AdvancedFireDetector advancedFireDetector = new AdvancedFireDetector();
+
+        System.out.println("----------------------");
+        fireDetector.detect();
+        advancedFireDetector.detect();
 
         FactoryCam factoryCam = new FactoryCam();
         factoryCam.setDetector(advancedFireDetector);
         factoryCam.setReporter(videoReporter);
 
+        System.out.println("----------------------");
         factoryCam.detect();
         factoryCam.report();
 
